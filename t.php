48f857519a1d6a1775bc0b84bd65e2e5972170f0
root <root@support@lockwoodcommercial.com>
2015-04-11 17:48:36 +0000

    initial, very few changes from base

---
+++ t.php
@@ -0,0 +1,11 @@
+HI
+<?php
+ $to = "sponrad@gmail.com";
+ $subject = "TEST";
+ $body = "Hi,Did you get this??";
+ if (mail($to, $subject, $body)) {
+   echo("<p>Message successfully sent!</p>");
+  } else {
+   echo("<p>Message delivery failed...</p>");
+  }
+ ?>
